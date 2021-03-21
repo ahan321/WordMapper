6c2366ff9fa0307e22e8a9f804442a8ace72358e
ahan123 <ahanmalli123@gmail.com>
2021-03-21 19:23:36 +0530

    Completes the GUI for the application excluding the PDF converter.

--- Main.py
+++ Main.py
@@ -1,9 +1,8 @@
 import PySimpleGUI as gui
 
-gui.theme("DarkAmber")
+gui.theme("Black")
 
-
-layout = [[gui.Text("Please type in a word")],[gui.InputText()], [gui.Button("Okay"),gui.Button("Cancel")]]
+layout = [[gui.Text("Please type in a word")],[gui.InputText()], [gui.Button("Okay"),gui.Button("Cancel"),gui.Text(text = "", key = "ERROR", size = (28,1))]]
 
 window = gui.Window("WordMapper", layout)
 
@@ -13,18 +12,24 @@ while True:
 
     isWord = True
     for i in values[0]:
-        if (i < ord("a") and i > ord("Z")) or (i < ord("A")) or (i > ord("z")):
+        if (ord(i) < ord("a") and ord(i) > ord("Z")) or (ord(i) < ord("A")) or (ord(i) > ord("z")):
             isWord = False
     
     if event == gui.WIN_CLOSED or event == "Cancel":
         shouldContinue = False
+    elif values[0] == "" or isWord == False:
+        window["ERROR"].update(value = "You have not entered a word.")
+        print(layout)
     elif event == "Okay":
         word = values[0]
+        shouldContinue = True
         break
-    elif values[0] == "":
-        layout.append(gui.Text(text="You have not "))
+
+window.close()
 
 if shouldContinue:
-    lol = True
+    
+    mindMapLayout = []
+    mindMapWindow = gui.Window("WordMapper", mindMapLayout)
 
 print(word)
